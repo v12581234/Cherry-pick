0d78f2cf8f7b5babc6afddab62a92b9b362ae876
v12581234 <vinhnguyen125812@gmail.com>
2025-07-05 02:54:45 +0700

    123

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ public class main {
         System.out.println("123");
         System.out.println("123345");
         int a=1;
-        int b=2;
-        int c=a+b;
-        System.out.println(c);
+
+  
+
     }
 }
